492fc3da7951d6427da5ca12b50e857c22754031
ejehameh <ejehameh01@gmail.com>
2026-02-10 13:42:43 +0100

    Enhance error handling in EditClientUserForm and update notification pop-up. Refactor SubmitButton styles in ViewLocations. Add forceLoginBranch method in APIService. Modify error response structure in clientUser actions.

--- src/Pages/OverviewPages/ClientUsers.js
+++ src/Pages/OverviewPages/ClientUsers.js
@@ -216,7 +216,7 @@ function ClientUsers(props) {
             cancelText="No"
           >
             {/* <CloseOutlined style={{ color: 'red', cursor: 'pointer' }} /> */}
-            <Button style={{ background:'#C72525', color: 'white', cursor: 'pointer' }}>delete</Button>
+            <Button style={{ border:'1px solid #C72525', color: '#C72525', cursor: 'pointer' }}>Remove</Button>
           </Popconfirm>
         ),
       },
