74083a8546d523069d25d182e3fa6146b1744f93
ejehameh <ejehameh01@gmail.com>
2026-06-22 10:01:16 +0100

    Refactor investor sidebar styles and structure

--- src/components/sideBar/InvestorSideMenu.js
+++ src/components/sideBar/InvestorSideMenu.js
@@ -148,17 +148,19 @@ function InvestorSideMenu({ collapsed, setCollapsed, logUserOut, basePath = "" }
   const MenuContent = () => (
     <div className="investor-sider-inner">
       <div className="investor-sider-top">
-        <div className="wyre-logo investor-sider-brand" style={{ textAlign: "center" }}>
-          <Image width={80} preview={false} src="/Images/Wyre white-08 1.png" />
-          {!collapsed && (
-            <div className="investor-sider-role-pill">Investor</div>
-          )}
+        <div className={`investor-sider-brand${collapsed && !isMobile ? " investor-sider-brand--collapsed" : ""}`}>
+          <div className="investor-sider-brand-logo">
+            <Image width={collapsed && !isMobile ? 32 : 70} preview={false} src="/Images/Wyre white-08 1.png" />
+            {!collapsed && (
+              <div className="investor-sider-role-pill">Investor</div>
+            )}
+          </div>
           {!isMobile && (
             <Button
               type="text"
               icon={<MenuOutlined style={{ color: "white" }} />}
               onClick={() => setCollapsed(!collapsed)}
-              style={{ marginLeft: 10, color: "white" }}
+              className="investor-sider-toggle"
             />
           )}
         </div>
